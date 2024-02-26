83c3e4baff0dfe94a8d0705ddbd5251d076203f4
jairocomino <jairocominoruiz@gmail.com>
2024-02-26 13:35:47 +0100

    pagina de contacto y error echas

--- src/components/pages/Contacto.jsx
+++ src/components/pages/Contacto.jsx
@@ -2,7 +2,19 @@
 
 const Contacto = () => {
   return (
-    <div>Contacto</div>
+    <>
+  <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden md:max-w-2xl">
+    <h2 className="text-3xl font-bold text-center mt-8 mb-4">Contacto</h2>
+    <div className="px-6 py-4">
+      <input type="text" placeholder="Correo Electrónico" className="w-full border border-gray-300 rounded-md px-3 py-2 mb-4 focus:outline-none focus:ring focus:ring-blue-400" />
+      <textarea cols="30" rows="10" placeholder="Pregunta aquí" className="w-full border border-gray-300 rounded-md px-3 py-2 mb-4 focus:outline-none focus:ring focus:ring-blue-400"></textarea>
+      <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-400">Enviar</button>
+    </div>
+    <div className="px-6 py-4">
+      <img src="../public/img/logonegativo" alt="" className="mx-auto" />
+    </div>
+  </div>
+</>
   )
 }
 
--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -2,7 +2,15 @@
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className="max-w-md p-8 bg-white rounded-lg shadow-lg">
+      <h2 className="text-3xl font-bold text-red-600 mb-4">Error 404</h2>
+      <p className="text-gray-700 text-lg mb-8">Lo sentimos, la página que estás buscando no se encontró.</p>
+      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring focus:ring-blue-400">
+        Volver a Inicio
+      </button>
+    </div>
+  </div>
   )
 }
 
